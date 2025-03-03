542a76192a0f94e01dc803e8cd98b8a9baba2d61
Kasper Rosenkrands (KVV) <KVV@kmd.dk>
2025-03-03 21:20:48 +0100

    updates to the homepage

--- vite-project/src/Pages/Home.tsx
+++ vite-project/src/Pages/Home.tsx
@@ -13,26 +13,27 @@ export default function Home() {
             alignItems={"center"}
           >
             <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-              Sample App
+              <Link href="/" underline="none" color="inherit">
+                Sample App
+              </Link>
             </Typography>
-            <Stack direction={"row"} spacing={2}>
-              <Link href="/" underline="hover" sx={{ minWidth: 100 }}>
-                Home
-              </Link>
-              <Link href="/" underline="hover" sx={{ minWidth: 100 }}>
-                Contact
-              </Link>
-              <Link href="/" underline="hover" sx={{ minWidth: 100 }}>
-                About Us
-              </Link>
-            </Stack>
+            <Link
+              href="/"
+              underline="none"
+              color="inherit"
+              sx={{ minWidth: 100 }}
+            >
+              Logout
+            </Link>
           </Stack>
         </Box>
         <Typography variant="body1" component="p" sx={{ mb: 2 }}>
           Welcome to the Sample App! This application is designed to demonstrate
           the integration of Material-UI components with a React application.
           Explore the various features and components that we have implemented
-          to help you get started with your own projects.
+          to help you get started with your own projects. Check out the new
+          additions to the repository, including the ASP.NET Core backend,
+          Docker setup, and database migrations, for more examples and features.
         </Typography>
         <Copyright />
       </Box>
