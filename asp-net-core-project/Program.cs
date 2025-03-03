44aaaab9dd1ce5d642e95dbfe47532d1431ea22a
Kasper Rosenkrands (KVV) <KVV@kmd.dk>
2025-03-03 20:13:45 +0100

    working setup with asp net core backend and docker compose

--- asp-net-core-project/Program.cs
+++ asp-net-core-project/Program.cs
@@ -1,8 +1,34 @@
+using asp_net_core_project.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var connectionString = $"Server={Environment.GetEnvironmentVariable("POSTGRES_SERVER")};" +
+                       $"Port={Environment.GetEnvironmentVariable("POSTGRES_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" +
+                       $"User Id={Environment.GetEnvironmentVariable("POSTGRES_USER")};" +
+                       $"Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")};";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -10,10 +36,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 }
 
+app.MapIdentityApi<IdentityUser>();
 app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
-
 app.UseHttpsRedirection();
 
 app.Run();
